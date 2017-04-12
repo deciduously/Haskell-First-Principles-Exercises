2c750f53b63cd52320b59458180e6e6fe0326c43
deciduously <bendlovy@gmail.com>
2017-04-12 11:05:04 -0400

    Optional Monoid

--- 5-Common Structures/15-Monoid, Semigroup/code/optional.hs
+++ 5-Common Structures/15-Monoid, Semigroup/code/optional.hs
@@ -1,8 +1,15 @@
-#### Optional Monoid
-```haskell
+--optional.hs
+module Optional where
+
+import Data.Monoid
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
   mappend Nada x = x
   mappend x Nada = x
   mappend (Only x) (Only y) = Only (x `mappend` y)
-```
